59edd8d91e1417447408a54fe0429ff8c0878042
hello <sudanshakya15@gmail.com>
2022-07-06 08:43:01 +0545

    Made dashboard for system

---
+++ database/migrations/2022_07_04_014212_create_students_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->string('mobile', 10) -> unique();
+            $table->string('email') -> unique();
+            $table->string('citizenship', 20) -> unique();
+            $table->string('gender', 1);
+            $table->string('blood_group', 3);
+            $table->string('perm_address');
+            $table->string('temp_address') -> nullable();
+            $table->date('dob');
+            $table->boolean('is_active') -> default(true);
+            $table->boolean('is_almuni');
+            $table->string('picture') -> nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
